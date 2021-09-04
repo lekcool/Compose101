e913b687f0159e3b0e17e2e082078585a21c7e56
Rattana Thanadka <rattanath@ch7.com>
2021-09-04 19:35:49 +0700

    chore: init project

---
+++ app/src/main/java/com/lkdev/compose101/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.lkdev.compose101
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.lkdev.compose101.ui.theme.Compose101Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Compose101Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Compose101Theme {
+        Greeting("Android")
+    }
+}
